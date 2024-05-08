3b0bce0d1cdcdff4e725c2f780333853ccac8cb0
Danielle Church <dani.church@gmail.com>
2024-05-08 14:02:23 -0400

    Add project files.

---
+++ middleclicktostow/ItemSlotPatches.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using System.Text;
+using Vintagestory.API.Common;
+
+namespace middleclicktostow
+{
+    [HarmonyPatch(typeof(ItemSlot))]
+    public static class ItemSlotPatches
+    {
+        public static ICoreAPI Api => MiddleClickToStowModSystem.Api;
+
+        [HarmonyPostfix, HarmonyPatch("ActivateSlotMiddleClick")]
+        public static void Before__ActivateSlotMiddleClick(ItemSlot __instance, ItemSlot sinkSlot, ref ItemStackMoveOperation op)
+        {
+            // from InventoryBase.ActivateSlot()
+            ItemSlot sourceSlot = __instance;
+            if (sourceSlot.Empty) return;
+            string? stackName = sourceSlot.Itemstack?.GetName();
+            string? sourceInv = sourceSlot.Inventory?.InventoryID;
+
+            StringBuilder shiftClickDebugText = new StringBuilder();
+
+            op.RequestedQuantity = sourceSlot.StackSize;
+            op.ActingPlayer.InventoryManager.TryTransferAway(sourceSlot, ref op, true, shiftClickDebugText);
+
+            Api.World.Logger.Audit("{0} shift clicked slot in {1}. Moved {2}x{3} to ({4})", op.ActingPlayer?.PlayerName, sourceInv, op.MovedQuantity, stackName, shiftClickDebugText.ToString());
+        }
+    }
+}
---
+++ middleclicktostow/MiddleClickToStowModSystem.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using System;
+using Vintagestory.API.Common;
+
+namespace middleclicktostow
+{
+    public class MiddleClickToStowModSystem : ModSystem
+    {
+        public override bool ShouldLoad(EnumAppSide forSide) => forSide == EnumAppSide.Client;
+
+        private static ICoreAPI? _api;
+        public static ICoreAPI Api => _api ?? throw new ArgumentNullException(nameof(Api));
+        public Harmony? harmony;
+
+        public override void Start(ICoreAPI api)
+        {
+            _api = api;
+
+            if (!Harmony.HasAnyPatches(Mod.Info.ModID)) {
+                harmony = new Harmony(Mod.Info.ModID);
+                harmony.PatchAll(); // Applies all harmony patches
+            }
+        }
+
+        public override void Dispose()
+        {
+            harmony?.UnpatchAll(Mod.Info.ModID);
+            harmony = null;
+        }
+    }
+}
